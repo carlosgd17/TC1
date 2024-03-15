423d812512b02bc805324f8c29487edc68a40a06
CarlosGD17 <carlosgd_17@outlook.com>
2024-03-15 16:37:28 -0600

    table inserts

--- db/db.sql
+++ db/db.sql
@@ -3,3 +3,8 @@ CREATE TABLE alumno(
     nombre VARCHAR 
 );
 
+INSERT INTO alumno (nombre) VALUES ('John Doe');
+INSERT INTO alumno (nombre) VALUES ('Jane Smith');
+INSERT INTO alumno (nombre) VALUES ('Robert Johnson');
+INSERT INTO alumno (nombre) VALUES ('Emily Davis');
+INSERT INTO alumno (nombre) VALUES ('James Brown');
